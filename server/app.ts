5bd441463fe16772cc21779827c1d39a2ba5c2d8
fenricage <fenricage@gmail.com>
2020-08-07 19:26:45 +0300

    SETUP TS FOR DEV

--- server/app.ts
+++ server/app.ts
@@ -14,12 +14,13 @@ const nextRoutes = require('../public/routes');
 const app = express();
 const router = express.Router();
 
+const test: string = "asg";
 
 
 
 const url = process.env.MONGODB_URI || config.db;
 const dev = process.env.NODE_ENV !== 'production';
-const nextApp = next({ dir: './public', dev }); // dir - indicate where pages, its detect from node start process dir
+const nextApp = next({ dir: './public', dev }); // dir - indicate where pages, its detects from node start process dir
 const handler = nextRoutes.getRequestHandler(nextApp); // part of next config
 
 
