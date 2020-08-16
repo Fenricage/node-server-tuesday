79a2d35c12f4daf825241770b100e331d81e11c3
fenricage <fenricage@gmail.com>
2020-08-16 03:00:42 +0300

    allow jsx in tsx, split functional and styles rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,35 @@
+const functionalRules = {
+  // FUNCTIONAL RULES
+  // check nextjs's link, for attributes href, to
+  'jsx-a11y/anchor-is-valid': [
+    'error',
+    {
+      components: ['Link'],
+      specialLink: ['to'],
+    },
+  ],
+  // checks that condition's value is first, then variable
+  // if ("red" === color) {}
+  'yoda': [
+    'error',
+    'always',
+    {
+      onlyEquality: true,
+    },
+  ],
+  'no-underscore-dangle': 'error', // regulates underscore symbol before var, methods names (like a _foo, _tmp)
+  'no-plusplus': 'off', // regulates unary operators -- and  ++
+  'react-hooks/rules-of-hooks': 'error', // hook's rules for fill effect dep
+  'react-hooks/exhaustive-deps': 'warn', // hook's rules for fill effect dep
+  'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx', '.tsx'] }],
+}
+
+const stylesRules = {
+  'array-bracket-spacing': ['error', 'always'],
+  'arrow-body-style': 'off',
+  'padded-blocks': 'off',
+}
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   env: {
@@ -24,31 +56,7 @@ module.exports = {
   plugins: ['react', 'react-hooks'],
   // adds and overrides rules
   rules: {
-    // FUNCTIONAL RULES
-    // check nextjs's link, for attributes href, to
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['to'],
-      },
-    ],
-    // checks that condition's value is first, then variable
-    // if ("red" === color) {}
-    'yoda': [
-      'error',
-      'always',
-      {
-        onlyEquality: true,
-      },
-    ],
-    'no-underscore-dangle': 'error', // regulates underscore symbol before var, methods names (like a _foo, _tmp)
-    'no-plusplus': 'off', // regulates unary operators -- and  ++
-    'react-hooks/rules-of-hooks': 'error', // hook's rules for fill effect dep
-    'react-hooks/exhaustive-deps': 'warn', // hook's rules for fill effect dep
-    // STYLES RULES
-    'array-bracket-spacing': ['error', 'always'],
-    'arrow-body-style': 'off',
-    'padded-blocks': 'off',
+    ...functionalRules,
+    ...stylesRules
   },
 };
