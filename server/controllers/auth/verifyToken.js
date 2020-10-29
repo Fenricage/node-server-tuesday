a04426248bc125fe8c89b9c625943a1b7bc6bb06
fenricage <fenricage@gmail.com>
2020-10-29 10:20:53 +0300

    clean

--- server/controllers/auth/verifyToken.js
+++ server/controllers/auth/verifyToken.js
@@ -12,12 +12,10 @@ const verifyToken = (req, res, next) => {
 
   // verifies secret and checks exp
   jwt.verify(token, config.secret, (err, decoded) => {
-    // console.log('\x1b[36m', 'decoded', decoded, '\x1b[0m');
 
     if (!decoded) {
       return res
         .status(403)
-        // .status(403)
         .send({ auth: false, message: 'Failed to authenticate token.' });
     }
 
